d38f129c8f483f19296ed6fe42f684888785a9b1
mabsboza <marcosboza@moi-solutions.com>
2021-03-09 21:58:36 -0600

    Add connections

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ app.use(express.static(__dirname + '/public'))
 //Conections
 io.on('connection', (socket) => { 
     socket.emit('connection_server', { mesg: 'Welcome', date: new Date() });
-    socket.on('connection_client', (data) => {
-        console.log(data);
-    })
+    socket.on('connection_server', (data) =>{
+        io.emit('connection_to_server_client', data);
+    });
 });
+
 server.listen(8080, ()=> {
     console.log('Server Up');
 });
